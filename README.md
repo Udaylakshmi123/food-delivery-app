5d7c6c84636e13d996360ec18319bf4b5e97944e
Udaylakshmi123 <udaylakshmi789@gmail.com>
2025-02-07 20:46:10 +1100

    Feb7

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-#Welcome to React#
+# Welcome to React #
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster Builds
+- Image optimization
+- Minification of the files
+- Bundling
+- Compress
+- Consistent Hashing 
+- Code Splitting
+- Differential Bundling - Support older browsers
+- Diagnostic
+- Error Handling
+- HTTPS also it will post (npx parcel index.html --https)->it load application in https server
+- Tree Shaking - will remove unused code for u
+- Different dev and prod bundle
+ 
--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-// const heading = React.createElement('h1',{id:'heading'},'Hello World from React');
-// console.log(heading); //its an object 
-// const root = ReactDOM.createRoot(document.querySelector('#root'));
-// console.log(root); //getting root id root
-// root.render(heading); //here it will convert heading object to h1 tag by using render
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parentTag = React.createElement('div',
     {id:'parent'},
     React.createElement('div',
         {id:'child'},
-        [React.createElement('h1',{id:'heading'},'Hello World H1'),React.createElement('h2',{id:'heading'},'Hello World H2')]));
+        [React.createElement('h1',{id:'heading'},'Hello World H1 tag'),React.createElement('h2',{id:'heading'},'Hello World H2')]));
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(parentTag);
